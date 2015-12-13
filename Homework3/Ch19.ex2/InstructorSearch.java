68aaaa521626933d4f4c3c096dc1511b4a476b6d
JGroot <GesikaMarie@gmail.com>
2015-12-12 21:12:00 -0500

    Adding Stock class

--- Homework3/Ch19.ex2/InstructorSearch.java
+++ Homework3/Ch19.ex2/InstructorSearch.java
@@ -1,15 +1,20 @@
 /*
-Chapter 9 presented an Instructor class that 
-holds information about an instructor.
-(See Section 9.7—Aggregation) Modify the 
-Instructor class by overriding the hashCode
-and equals methods. Then write a class that
-stores several Instructor objects in a
-HashSet. The class should be able to
-display all the instructors in the set, 
-and allow the user to search for an 
-instructor. Demonstrate the class in an
-application.
+      Jessica Groot
+      12/07/2015
+      Chapter 19, Challange 2
+      Java II
+      
+      Chapter 9 presented an Instructor class that 
+      holds information about an instructor.
+      (See Section 9.7—Aggregation) Modify the 
+      Instructor class by overriding the hashCode
+      and equals methods. Then write a class that
+      stores several Instructor objects in a
+      HashSet. The class should be able to
+      display all the instructors in the set, 
+      and allow the user to search for an 
+      instructor. Demonstrate the class in an
+      application.
 */
 import java.util.*;
 import javafx.application.*;
---
+++ Homework3/Ch19.ex4/Stock.java
@@ -0,0 +1,39 @@
+/*
+Chapter 9 presented a Stock class that holds 
+information about a stock. (See Section 9.4— 
+The toString Method) Write a class that keeps 
+Stock objects in a Map. The class should be 
+able to retrieve a particular Stock object 
+from the Map by searching on its stock symbol.
+Demonstrate the class in an application.
+ 
+*/
+
+public class Stock
+{
+   private String symbol;
+   private double shareprice;
+   
+   public Stock(String sym, double sp)
+   {
+      symbol = sym;
+      shareprice = sp;
+   }
+   
+   public String getSymbol()
+   {
+      return symbol;
+   }
+   
+   public double getShareprice()
+   {
+      return shareprice;
+   }
+   
+   public String toString()
+   {
+      String str = "Trading symbol: " + symbol +
+                  "\nShareprice: " + shareprice;
+      return str;
+   }
+}
